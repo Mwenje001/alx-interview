46ce56eb9cd0888f4d390f4510060b61ae67b4eb
root <root@DESKTOP-EK7JHBM>
2023-08-10 11:51:40 +0300

    Adds Tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,31 @@
 #!/usr/bin/node
-/**
- * a script that prints all characters of a Star Wars movie
- * use star wars api
- */
-const request = require('request');
-const film_id = process.argv[2];
-if (!film_id  || isNaN(film_id)) {
-  process.exit(1);
-}
-const url = `https://swapi-api.hbtn.io/api/films/${film_id}`;
 
-request(url, (error, res, body) => {
-  if (error) {
-    console.log(error);
+const request = require('request');
+
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
     return;
   }
-  const characterList = [];
 
-  const json = JSON.parse(body);
-  const characters = json.characters;
-
-  characters.forEach((character) => {
-    const url = character;
-    const promise = new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        const json = JSON.parse(body);
-        resolve(json.name);
-      });
-    });
-    characterList.push(promise);
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
   });
-  Promise.all(characterList).then((values) => {
-    values.forEach((value) => {
-      console.log(value);
-    });
-  }).catch((error) => {
+}
+
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
     console.log(error);
-  });
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
 });
